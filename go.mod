b92b82f89da4574e4b49e5d668da401a5db9ae35
Galeev Timur <timurdev@whitelevel.eu>
2024-01-03 20:17:40 +0300

    Revert "fix"
    This reverts commit 5f3a46d8f8a070d0435bb5308364023cae9ae5c9.

--- go.mod
+++ go.mod
@@ -5,20 +5,17 @@ go 1.21.1
 require (
 	github.com/caarlos0/env v3.5.0+incompatible
 	github.com/go-chi/chi v1.5.5
-	github.com/go-chi/chi/v5 v5.0.11
 	github.com/gojek/heimdall/v7 v7.0.2
-	github.com/rs/zerolog v1.31.0
 	github.com/stretchr/testify v1.8.4
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gojek/valkyrie v0.0.0-20180215180059-6aee720afcdf // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
-	golang.org/x/sys v0.12.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/server/handler/handlerLogger.go
+++ internal/server/handler/handlerLogger.go
@@ -1,54 +1,79 @@
 package handler
 
 import (
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/rs/zerolog"
+	"go.uber.org/zap"
 	"net/http"
-	"os"
+	"sync"
 	"time"
 )
 
-var (
-	Log *zerolog.Logger
+type (
+	// берём структуру для хранения сведений об ответе
+	responseData struct {
+		status int
+		size   int
+	}
+
+	// добавляем реализацию http.ResponseWriter
+	loggingResponseWriter struct {
+		http.ResponseWriter // встраиваем оригинальный http.ResponseWriter
+		responseData        *responseData
+	}
 )
 
-func init() {
-	zerolog.SetGlobalLevel(zerolog.InfoLevel)
-	zerolog.TimeFieldFormat = zerolog.TimeFormatUnixMs
-	zerolog.DurationFieldUnit = time.Millisecond
-	consoleLog := zerolog.ConsoleWriter{
-		Out:        os.Stdout,
-		NoColor:    false,
-		TimeFormat: time.DateTime + ".000",
-	}
-	l := zerolog.New(consoleLog).With().Timestamp().Caller().Logger()
-	Log = &l
+var (
+	sLog *zap.SugaredLogger
+	once sync.Once
+)
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	// записываем ответ, используя оригинальный http.ResponseWriter
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size // захватываем размер
+	return size, err
 }
 
-func SetLevel(s string) {
-	v, err := zerolog.ParseLevel(s)
-	if err != nil {
-		Log.Warn().Err(err).Msg("invalid log level specified")
-	}
-	zerolog.SetGlobalLevel(v)
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	// записываем код статуса, используя оригинальный http.ResponseWriter
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode // захватываем код статуса
+}
+
+func getLogger() *zap.SugaredLogger {
+	once.Do(func() {
+		logger, err := zap.NewDevelopment()
+		if err != nil {
+			panic(err)
+		}
+		sLog = logger.Sugar()
+	})
+	return sLog
 }
 
 func LoggerHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		// to get response data
-		ww := middleware.NewWrapResponseWriter(writer, request.ProtoMajor)
-		tStart := time.Now()
-		defer func() {
-			dur := time.Since(tStart)
-			Log.Info().
-				Str("host", request.Host).
-				Str("url", request.URL.Path).
-				Str("method", request.Method).
-				Dur("ms_served", dur).
-				Int("status", ww.Status()).
-				Int("bytes_sent", ww.BytesWritten()).
-				Msg("http request")
-		}()
-		next.ServeHTTP(ww, request)
+		start := time.Now()
+
+		responseData := &responseData{
+			status: 0,
+			size:   0,
+		}
+		lw := loggingResponseWriter{
+			ResponseWriter: writer, // встраиваем оригинальный http.ResponseWriter
+			responseData:   responseData,
+		}
+		next.ServeHTTP(&lw, request) // внедряем реализацию http.ResponseWriter
+
+		duration := time.Since(start)
+
+		sugar := getLogger()
+
+		sugar.Infoln(
+			"uri", request.RequestURI,
+			"method", request.Method,
+			"status", responseData.status, // получаем перехваченный код статуса ответа
+			"duration", duration,
+			"size", responseData.size, // получаем перехваченный размер ответа
+		)
 	})
 }
