c5df89d1019cb21e69b77a7968082842adc121a1
Galeev Timur <timurdev@whitelevel.eu>
2024-01-03 19:33:16 +0300

    fix

--- internal/server/handler/handler_test.go
+++ internal/server/handler/handler_test.go
@@ -1,9 +1,14 @@
 package handler
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/gtgaleevtimur/metrics-alertings/internal/server/entity"
 	"github.com/gtgaleevtimur/metrics-alertings/internal/server/repository"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -102,5 +107,64 @@ func TestGetMetric(t *testing.T) {
 		defer resp.Body.Close()
 		assert.Equal(t, "text/plain; charset=utf-8", resp.Header.Get("Content-Type"))
 		assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+	})
+}
+
+func TestUpdateMetricJSON(t *testing.T) {
+	t.Run("Test update counter metric", func(t *testing.T) {
+		controller := repository.NewServerMemStorage()
+		r := NewServerRouter(controller)
+		ts := httptest.NewServer(r)
+		defer ts.Close()
+		for i := 0; i < 5; i++ {
+			temp := int64(i)
+			m := entity.Metrics{
+				MType: "counter",
+				ID:    "test",
+				Delta: &temp,
+			}
+			body, err := json.Marshal(m)
+			require.NoError(t, err)
+			request, err := http.NewRequest(http.MethodPost, ts.URL+"/update/", bytes.NewReader(body))
+			request.Header.Set("Content-Type", "application/json")
+			require.NoError(t, err)
+			client := &http.Client{}
+			resp, err := client.Do(request)
+			require.NoError(t, err)
+			b, err := io.ReadAll(resp.Body)
+			require.NoError(t, err)
+			fmt.Println(string(b))
+			defer resp.Body.Close()
+			assert.Equal(t, "application/json", resp.Header.Get("Content-Type"))
+			assert.Equal(t, http.StatusOK, resp.StatusCode)
+		}
+	})
+	t.Run("Test update gauge metric", func(t *testing.T) {
+		controller := repository.NewServerMemStorage()
+		r := NewServerRouter(controller)
+		ts := httptest.NewServer(r)
+		defer ts.Close()
+		for i := 0; i < 5; i++ {
+			temp := float64(i)
+			m := entity.Metrics{
+				MType: "gauge",
+				ID:    "test",
+				Value: &temp,
+			}
+			body, err := json.Marshal(m)
+			require.NoError(t, err)
+			request, err := http.NewRequest(http.MethodPost, ts.URL+"/update/", bytes.NewReader(body))
+			request.Header.Set("Content-Type", "application/json")
+			require.NoError(t, err)
+			client := &http.Client{}
+			resp, err := client.Do(request)
+			require.NoError(t, err)
+			b, err := io.ReadAll(resp.Body)
+			require.NoError(t, err)
+			fmt.Println(string(b))
+			defer resp.Body.Close()
+			assert.Equal(t, "application/json", resp.Header.Get("Content-Type"))
+			assert.Equal(t, http.StatusOK, resp.StatusCode)
+		}
 	})
 }
